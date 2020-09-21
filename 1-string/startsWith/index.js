42ecff1477f121423ca18384d8d53c728a567f05
Tiedapang <2750428832@qq.com>
2020-09-21 18:28:17 +0800

    forget commit this file

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,6 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let startWithANumbers = [];
+  startWithANumbers = collection.filter(item => item.startsWith('粤A'));
+  return startWithANumbers.length;
 }
